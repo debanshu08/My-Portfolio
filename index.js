f4a6f4048f814bba4c8eeb73f4b6ef4f79a8fa25
debanshu08 <debanshumutt1882000@gmail.com>
2020-09-09 23:29:15 +0530

    Added Required Files.

---
+++ index.js
@@ -0,0 +1,17 @@
+const Skills = [
+  "B.Tech Under Graduate",
+  "Front-End Web Developer",
+  "Java Programmer",
+];
+let index = 0,
+  skillIndex = 0,
+  skill = "";
+!(function e() {
+  skillIndex === Skills.length && (skillIndex = 0);
+  let l = (skill = Skills[skillIndex]).slice(0, ++index);
+  console.log(l),
+    (document.querySelector(".type-text").textContent = l),
+    l.length === skill.length
+      ? (skillIndex++, (index = 0), setTimeout(e, 2e3))
+      : setTimeout(e, 150);
+})();
